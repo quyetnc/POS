ded19ecefac4dfee8e9fe22eebd6ef9071cde757
Minh <nghiemminh1904@gmail.com>
2020-10-09 13:58:54 +0700

    Minh Push

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,20 +1,43 @@
-
-
-
 import React from 'react';
-import { Button, Image, View, Text } from 'react-native';
+import { Button, Image, View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
-
+import { Sizes } from '@dungdang/react-native-basic'
 export default class Home extends React.Component {
   render() {
     return (
       <SafeAreaView>
-
-        <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center', backgroundColor: "#eee" }}>
-          <Text  > Home!</Text>
+        <View style={styles.header}>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={styles.titileHeader}> Home </Text>
+          </View>
         </View>
-
+        <View style={styles.container}>
+          <Text> Home!</Text>
+        </View>
       </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create = ({
+  header: {
+    width: '100%',
+    height: Sizes.s100,
+    backgroundColor: '#4ebd73',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: Sizes.s10,
+  },
+  titileHeader: {
+    color: 'white',
+    fontSize: Sizes.h28,
+    fontWeight: 'bold'
+  },
+  container: {
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff'
+  }
+})

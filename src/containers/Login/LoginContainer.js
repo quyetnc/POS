cee98f4511319715ab615a1a48df89e57462fe44
Quyet Nguyen Canh (FIS TDC HCM) <quyetnc2@fpt.com.vn>
2020-10-09 16:54:07 +0700

    NCQ

--- src/containers/Login/LoginContainer.js
+++ src/containers/Login/LoginContainer.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import LoginComponent from '../../components/Login/LoginComponent'
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import LoginComponent from '../../components/Login/LoginComponent';
+import {getAllPropertyAction} from '../../redux/actions/Login/LoginAction';
 
 export class LoginContainer extends Component {
-    render() {
-        return (
-           <LoginComponent {...this.props} />
-        )
-    }
+  render() {
+    return <LoginComponent {...this.props} />;
+  }
 }
 
-const mapStateToProps = (state) => ({
-    
-})
+const mapStateToProps = (state) => {
+  // console.log(state.allPropertyReducers);
+  return {
+    allPropertyReducers: state.allPropertyReducers,
+  };
+};
 
-const mapDispatchToProps = {
-    
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onGetAllPropertyAction: () => {
+      dispatch(getAllPropertyAction());
+    },
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer);
---
+++ src/redux/middleware/saga/Login/getAllPropertySaga.js
@@ -0,0 +1,29 @@
+import {
+  GET_ALLPROPERTY,
+  GET_ALLPROPERTY_SUCCESS,
+  GET_ALLPROPERTY_ERROR,
+} from '../../../actions/Login/LoginAction';
+
+import {call, takeEvery, put} from 'redux-saga/effects';
+import {get_AllProperty} from '../../api/Login/getAllPropertyApi';
+
+function* getAllProperty() {
+  let response = yield get_AllProperty();
+  // console.log(response);
+// console.log('ok')
+  //   const error = response.message;
+    if (response !== undefined) {
+      // if (response.resultCode === 1) {
+       
+        yield put({type: GET_ALLPROPERTY_SUCCESS, response: response});
+      // } else {
+      //   yield put({type: GET_ALLPROPERTY_ERROR, error});
+      // }
+    } else {
+      yield put({type: GET_ALLPROPERTY_ERROR, error});
+    }
+}
+
+export function* watchAllProperty() {
+  yield takeEvery(GET_ALLPROPERTY, getAllProperty);
+}

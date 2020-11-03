5a91d59a8d477c5b37761cd9f51e8e8727889fad
Minh <nghiemminh1904@gmail.com>
2020-11-03 10:32:28 +0700

    customDrawble

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Button, Image, View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { Image, View, Text, TouchableOpacity, StyleSheet, Switch } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { Sizes } from '@dungdang/react-native-basic';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import images from '../res/images';
 import LoginContainer from './Login/LoginContainer';
-import demo from '../components/Custom/demo';
-import demo2 from '../components/Custom/demo2';
 import HomeContainer from './Home/HomeContainer';
 import ActionContainer from './Action/ActionContainer';
 import SettingContainer from './Setting/SettingContainer';
@@ -18,30 +18,92 @@ import FindGuestContainer from './Action/FindGuestContainer';
 import FindTransactionContainer from './Action/FindTransactionContainer';
 import FillCodeContainer from './Login/FillCodeContainer';
 import ConfirmApiContainer from './Login/ConfirmApiContainer';
-import images from '../res/images';
-import { Sizes } from '@dungdang/react-native-basic'
+
+
+
+
+
+
 const Stack = createStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
 const Drawer = createDrawerNavigator();
-
+const styles = StyleSheet.create({
+  customMenu: {
+    flexDirection: 'row',
+    marginHorizontal: Sizes.s20,
+    height: Sizes.s200,
+    // borderBottomColor: '#E8E8E8',
+    // borderBottomWidth: 1,
+    alignItems: 'center'
+  },
+  headerMenu: {
+    flexDirection: 'row',
+    backgroundColor: '#1890FF',
+    height: Sizes.s140,
+    borderBottomColor: '#E8E8E8',
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  viewText: {
+    flexDirection: 'row',
+    height: Sizes.s200,
+    width: '86.5%',
+    borderBottomColor: '#E8E8E8',
+    borderBottomWidth: 1,
+    alignItems: 'center'
+  }
+})
 function CustomDrawerContent({ navigation }) {
+
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
-    // <Button
-    //   title="Go somewhere"
-    //   onPress={() => {
-    //     // Navigate using the `navigation` prop that you received
-    //     navigation.navigate('Action');
-    //   }}
-    // />
     <View>
-      <TouchableOpacity >
-        <Image source={images.ic_change_out_let} resizeMode='contain' style={{ width: Sizes.s25 }} />
-        <Text>Change outlet</Text>
+      <View style={styles.headerMenu}>
+        <Text style={{ color: '#FFFFFF', fontWeight: 'bold', fontSize: Sizes.h32, }}>Mở Rộng</Text>
+      </View>
+      <TouchableOpacity style={styles.customMenu}>
+        <Image source={images.ic_change_out_let} resizeMode='center' style={{ width: Sizes.s100 }} />
+        <View style={styles.viewText}>
+          <Text style={{ marginLeft: Sizes.s15 }}>Change outlet</Text>
+        </View>
       </TouchableOpacity>
-      <TouchableOpacity>
-        <Image source={images} />
-        <Text>Change outlet</Text>
+
+      <TouchableOpacity style={styles.customMenu}>
+        <Image source={images.ic_transaction} resizeMode='center' style={{ width: Sizes.s100 }} />
+        <View style={styles.viewText}>
+          <Text style={{ marginLeft: Sizes.s15 }}>Find transaction</Text>
+        </View>
       </TouchableOpacity>
+
+      <TouchableOpacity style={styles.customMenu}>
+        <Image source={images.ic_find_guest} resizeMode='center' style={{ width: Sizes.s100 }} />
+        <View style={styles.viewText}>
+          <Text style={{ marginLeft: Sizes.s15 }}>Find guest</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.customMenu}>
+        <Image source={images.ic_about} resizeMode='center' style={{ width: Sizes.s100 }} />
+        <View style={styles.viewText}>
+          <Text style={{ marginLeft: Sizes.s15 }}>About</Text>
+        </View>
+      </TouchableOpacity>
+
+      <View style={styles.customMenu}>
+        <Switch
+          trackColor={{ false: "#000", true: "#81b0ff" }}
+          thumbColor={isEnabled ? "#fff" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+        <View style={styles.viewText}>
+          <Text style={{ marginLeft: Sizes.s15 }}>Dark Mode</Text>
+        </View>
+      </View>
     </View>
 
 
